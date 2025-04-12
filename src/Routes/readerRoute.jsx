9a85b4e9e93d873e1a408b6212ce8a2ee280ee6d
sneha p <snehapdas777@gmail.com>
2025-04-12 18:34:58 +0530

    route setup changed

--- src/Routes/readerRoute.jsx
+++ src/Routes/readerRoute.jsx
@@ -8,7 +8,7 @@ function ReaderRoute() {
   return (
     <Routes>
       {/* Use absolute paths */}
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={<ReaderHomePage />} />
 
